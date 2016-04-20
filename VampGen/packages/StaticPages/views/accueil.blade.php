5375f7d0033ad9c2ddaeca0fa857f1cf18aa7d4a
MehdiHenna <medh31@gmail.com>
2016-04-20 16:54:30 +0200

    Form de la page d'accueil

--- VampGen/packages/StaticPages/views/accueil.blade.php
+++ VampGen/packages/StaticPages/views/accueil.blade.php
@@ -1,4 +1,26 @@
 @extends(config('pages.pages_template'))
 @section('content')
-	<h2>Moi aussi je suis la !</h2>
+	<p>Générateur pour la création de personnage de vampire</p>
+	<form action="" method="post" class="vg-form">
+		<div class="vg-field">
+		<label for="nom">Bonjour</label>
+		<input type="text" id="nom"/>
+		</div>
+		<div class="vg-field">
+		<label for="nom">Bonjour</label>
+		<input type="text" id="nom"/>
+		</div>
+		<div class="vg-field">
+		<label for="nom">Bonjour</label>
+		<input type="text" id="nom"/>
+		</div>
+		<div class="vg-field">
+		<label for="nom">Bonjour</label>
+		<input type="text" id="nom"/>
+		</div>
+		<div>
+			<button class="vg-button">envoyé</button>
+		</div>
+	</form>
+
 @endsection
