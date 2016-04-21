bd3ebe9c38fd21003a05243f0d8ed70dac097246
AllanCerveaux <cerveauxallanjean@gmail.com>
2016-04-21 10:59:34 +0200

    ajout des formualire dans la page bonnus et modification du titre dans step6

--- VampGen/packages/StaticPages/views/step7.blade.php
+++ VampGen/packages/StaticPages/views/step7.blade.php
@@ -1,6 +1,6 @@
 @extends(config('pages.pages_template'))
 @section('content')
-<h3>Historique</h3>
+<h3>Capacité</h3>
 <form action="" method="post" class="vg-form">
 	<div class="vg-field">
 		<label for="nom">Célérité</label>
