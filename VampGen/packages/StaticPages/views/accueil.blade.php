96d28d21415bfba2968a71080f21d17f462d927e
Andrea <andrea.buisset@hotmail.fr>
2016-04-22 15:20:30 +0200

    Ajout de boutons radios pour le choix du clan

--- VampGen/packages/StaticPages/views/accueil.blade.php
+++ VampGen/packages/StaticPages/views/accueil.blade.php
@@ -9,41 +9,88 @@
 	<form action="" method="post" class="vg-form">
 		<div class="vg-field">
 			<input type="text" id="nom" required autocomplete="off" />
-			<label for="nom">Nom</label>
+			<label for="nom" class="label-text">Nom</label>
+			<p class="info">C'est votre nom de joueur</p>
 		</div>
 		<div class="vg-field">
 			<input type="text" id="chronique" required autocomplete="off"/>
-			<label for="nom">Chronique</label>
+			<label for="nom" class="label-text">Chronique</label>
+			<p class="info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam, itaque, eius. Expedita sint est asperiores, perferendis doloribus placeat provident velit dicta cum iusto vero culpa, sed accusamus corrupti. Pariatur, voluptate?</p>
 		</div>
 		<div class="vg-field">
 			<input type="text" id="nature" required autocomplete="off"/>
-			<label for="nom">Nature</label>
+			<label for="nom" class="label-text">Nature</label>
 		</div>
 		<div class="vg-field">
 			<input type="text" id="attitude" required autocomplete="off"/>
-			<label for="nom">Attitude</label>
+			<label for="nom" class="label-text">Attitude</label>
 		</div>
 		<div class="vg-field">
 			<input type="text" id="concept" required autocomplete="off"/>
-			<label for="nom">Concept</label>
+			<label for="nom" class="label-text">Concept</label>
 		</div>
 		<div class="vg-field">
-			<div class="styled-select">
-				<select>
-					<option>Clan</option>
-					<option>Choice 1</option>
-					<option>Choice 2</option>
-					<option>Choice 3</option>
-				</select>
-			</div>
+			<p class="radio-list-title">Clans</p>
+				<div class="radio-input">
+					<input name="clan" type="radio" id="brujah" checked="checked">
+		   			<label for="brujah">Brujah</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="assamite">
+		   			<label for="assamite">Assamite</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="tremere">
+		   			<label for="tremere">Tremere</label>
+	   			</div>
+				<div class="radio-input">	   			
+		   			<input name="clan" type="radio" id="lasombra">
+		   			<label for="lasombra">Lasombra</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="followers_of_set">
+		   			<label for="followers_of_set">Disciple de Seth</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="malkavien">
+		   			<label for="malkavien">Malkavien</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="ventrue">
+		   			<label for="ventrue">Ventrue</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="toreador">
+		   			<label for="toreador">Toreador</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="tzimisce">
+		   			<label for="tzimisce">Tzimisce</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="nosferatu">
+		   			<label for="nosferatu">Nosferatu</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="giovanni">
+		   			<label for="giovanni">Giovanni</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="gangrel">
+		   			<label for="gangrel">Gangrel</label>
+	   			</div>
+				<div class="radio-input">
+		   			<input name="clan" type="radio" id="ravnos">
+		   			<label for="ravnos">Ravnos</label>
+	   			</div>
 		</div>
 		<div class="vg-field">
 			<input type="text" id="generation" required/>
-			<label for="nom">Génération</label>
+			<label for="nom" class="label-text">Génération</label>
 		</div>
 		<div class="vg-field">
 			<input type="text" id="refuge" required/>
-			<label for="nom">Refuge</label>
+			<label for="nom" class="label-text">Refuge</label>
 		</div>
 		<div class="center-aligned">
 			<button type="submit" class="vg-button">Envoyer</button>
