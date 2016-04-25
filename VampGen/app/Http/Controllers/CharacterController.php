df155f3d4b9b6f1c2e04dce48882b990fb652b91
MehdiHenna <medh31@gmail.com>
2016-04-25 11:45:54 +0200

    Ajout des controller qui récupére les infons des formulaire pour les clans, discipline skills et clan

--- VampGen/app/Http/Controllers/CharacterController.php
+++ VampGen/app/Http/Controllers/CharacterController.php
@@ -20,4 +20,28 @@ class CharacterController extends Controller
     		]);
     	return redirect('/profil');
     }
+
+    public function postClans(Request $request){
+        $data = Clans::create([
+            'id' => $request->get('id'),
+            'name' => $request->get('name'),
+            ])
+
+    }
+
+    public function PostDisciplines (Request $request){
+        $data= Disciplines::create([
+            'id' => $request->get('id'),
+            'name' => $request->get('name'),
+            'max' => $request->get('max'),
+            ]);
+    }
+
+    public function postSkills (Request $request) {
+        $data = Skills::create([
+            'id' => $resquest->get('id'),
+            'name' => $request->get('name'),
+            'max' => $request->get('max')
+            ]);
+    }
 }
