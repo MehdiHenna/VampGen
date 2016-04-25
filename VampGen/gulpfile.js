0fe20b350e9e46bf66d3b69989de928fe00aad5b
AllanCerveaux <cerveauxallanjean@gmail.com>
2016-04-25 10:27:46 +0200

    Ajout des variables de couleur.

--- VampGen/gulpfile.js
+++ VampGen/gulpfile.js
@@ -22,6 +22,6 @@ var browserify = ('browserify');
 
  elixir(function(mix){
  	mix.browserSync({
- 		proxy:'vampire.dev'
+ 		proxy:'vampgen.dev'
  	});
  });
---
+++ VampGen/packages/StaticPages/views/test.blade.php
@@ -0,0 +1,6 @@
+@extends(config('pages.pages_template'))
+@section('content')
+	<div class="center-aligned">
+		<div class="segment">test</div>
+	</div>
+@endsection
